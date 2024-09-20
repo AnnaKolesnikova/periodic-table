6c84e127383a63777bb76d33ca11883c133eebf6
anna <ani.kolesnikova@gmail.com>
2024-09-20 12:11:34 +0200

    feat: add filter

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -9,7 +9,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { PeriodicElement } from '../../shared/models/periodicElement.model';
 import { FormsModule } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { DialogComponent } from '../dialog/dialog.component';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-table',
@@ -21,6 +24,7 @@ import { DialogComponent } from '../dialog/dialog.component';
     MatInputModule,
     MatButtonModule,
     MatDialogModule,
+    MatProgressSpinnerModule,
     FormsModule,
   ],
   templateUrl: './table.component.html',
@@ -34,7 +38,8 @@ export class TableComponent implements OnInit {
     'symbol',
     'actions',
   ];
-  dataSource: PeriodicElement[] = [];
+  dataSource = new MatTableDataSource<PeriodicElement>([]);
+  filterSubject = new Subject<string>();
   loading: boolean = true;
 
   constructor(private dataService: DataService, public dialog: MatDialog) {}
@@ -42,7 +47,7 @@ export class TableComponent implements OnInit {
   ngOnInit(): void {
     this.dataService.fetchData().subscribe({
       next: (response) => {
-        this.dataSource = response;
+        this.dataSource.data = response;
         this.loading = false;
       },
       error: (error) => {
@@ -50,27 +55,33 @@ export class TableComponent implements OnInit {
         this.loading = false;
       },
     });
+
+    this.filterSubject.pipe(debounceTime(2000)).subscribe((filterValue) => {
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+    });
   }
 
   openEditDialog(element: PeriodicElement): void {
     const dialogRef = this.dialog.open(DialogComponent, {
       panelClass: 'custom-dialog',
-      data: { element: { ...element } }, // pass element's copy to dialog
+      data: { element: { ...element } },
     });
 
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        // find index in data copy
-        const index = this.dataSource.findIndex(
+        const index = this.dataSource.data.findIndex(
           (item) => item.name === element.name
         );
         if (index !== -1) {
-          // update element and create new link to data array
-          const updatedDataSource = [...this.dataSource];
-          updatedDataSource[index] = result;
-          this.dataSource = updatedDataSource; // create new link to data array
+          this.dataSource.data[index] = result;
+          this.dataSource._updateChangeSubscription();
         }
       }
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.filterSubject.next(filterValue);
   }
 }
