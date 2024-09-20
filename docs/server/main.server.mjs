3ac8cb9d85279367fea2fa5b11eb5d4b31ff35c1
anna <ani.kolesnikova@gmail.com>
2024-09-20 12:12:52 +0200

    update build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DCWO2YZD.mjs";import"./chunk-KAMO7WYR.mjs";import"./chunk-6JYNCRYE.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-BIP6KWYZ.mjs";import"./chunk-UZFIR7UJ.mjs";import"./chunk-CPQL4C7I.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
